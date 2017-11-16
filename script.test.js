88f9ea236c7049c4b8d7bb03e8533a614a599e8a
andres9722 <andres_9722@outlook.com>
2017-11-16 16:17:01 -0500

    test

--- script.test.js
+++ script.test.js
@@ -10,115 +10,6 @@ import {
     printOutPersonAge
 } from './script.js'
 
-/*test('should have "r", "g" and "b" as properties with numbers as values', () => {
-    expect(generateRandomColor()).toMatchObject(expect.objectContaining({
-        r: expect.any(Number),
-        g: expect.any(Number),
-        b: expect.any(Number)
-    }))
-})
-
-test('the numbers should not have decimal numbers', () => {
-    const values = generateRandomColor()
-
-    for (let prop in values) {
-        expect(values[prop] % 1 === 0).toBeTruthy()
-    }
-})
-
-test('the numbers should be less than 255 and greater than 0', () => {
-    const values = generateRandomColor()
-    
-    for (let prop in values) {
-        expect(values[prop]).toBeGreaterThanOrEqual(0)
-        expect(values[prop]).toBeLessThanOrEqual(255)
-    }
-})
-
-test('should return a new array without negative numbers', () => {
-    const arr = [-4, -5, -56, -12, -6, 7, 8, -5, 3, 9, 0]
-    expect(filterNegativeNumbers(arr)).toEqual([7, 8, 3, 9, 0])
-    expect(filterNegativeNumbers([0])).toEqual([0])
-    expect(functionalFilterNegativeNumbers([0])).toEqual([0])
-})
-
-test('should return an empty array if all the values are negative', () => {
-    expect(filterNegativeNumbers([-5])).toEqual([])
-    expect(functionalFilterNegativeNumbers([-5])).toEqual([])
-})
-
-test('should return an empty array if the original array is empty', () => {
-    expect(filterNegativeNumbers([])).toEqual([])
-    expect(functionalFilterNegativeNumbers([])).toEqual([])
-})
-
-test('should not modify the original array', () => {
-    const arr = [-5, 7, 5]
-    const arrCopy = arr
-    console.log(arrCopy)
-    filterNegativeNumbers(arr)
-    expect(arr).toEqual(arrCopy)
-})
-
-test('should return a new array with strings', () => {
-    const arr = [-5, 7, 5]
-    expect(mapNumbersIntoStrings(arr)).toEqual(['-5', '7', '5'])
-    expect(functionalMapNumbersIntoStrings(arr)).toEqual(['-5', '7', '5'])
-})
-
-test('should return an empty array if the original array is empty', () => {
-    expect(mapNumbersIntoStrings([])).toEqual([]);
-    expect(functionalMapNumbersIntoStrings([])).toEqual([]);
-})
-
-test('should not modify the original array', () => {
-    const arr = [-5, 7, 5]
-    const arrCopy = arr
-    mapNumbersIntoStrings(arr)
-    expect(arr).toEqual(arrCopy)
-})
-
-beforeEach(() => {
-    global.console = {
-        log: jest.fn()
-    }
-})
-
-test('should print in console the type', () => {
-    printType(1);
-    expect(global.console.log).toHaveBeenCalledWith('number')
-
-    printType('hi');
-    expect(global.console.log).toHaveBeenCalledWith('string')
-})
-
-test('should return true with a palindrome word', () => {
-    expect(isPalindrome('ana')).toBeTruthy()
-    expect(isPalindrome('oso')).toBeTruthy()
-    expect(isPalindrome('%AniTA LAVa& LA tiNA')).toBeTruthy()
-})
-
-let personInstance
-
-beforeAll(() => {
-    personInstance = new Person('Andres', 19)
-
-    global.console = {
-        log: jest.fn()
-    }
-})
-
-test('should return an instance with the correct values when calling it as a constructor', () => {
-    expect(personInstance).toMatchObject({
-        name: 'Andres',
-        age: 19
-    })
-})
-
-test('should print in console the age of the given instance', () => {
-    printOutPersonAge(new Person('Andres', 19))
-    expect(global.console.log).toHaveBeenCalledWith(19)
-})*/
 
 describe('generateRandomColor', () => {
     it('should have "r", "g" and "b" as properties with numbers as values', () => {
@@ -145,12 +36,12 @@ describe('generateRandomColor', () => {
         expect(values.g % 1 == 0).toBeTruthy()
         expect(values.b % 1 == 0).toBeTruthy()
     })
+});
 
-
-/*describe('filterNegativeNumbers', () => {
+describe('filterNegativeNumbers', () => {
     it('should return a new array without negative numbers', () => {
-        const arr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-        expect(filterNegativeNumbers(arr)).toEqual([0, 1, 2, 3, 4, 5])
+        const arr = [-7, -89, -67, -4, -1, 0, 11, 34, 3, 4, 5]
+        expect(filterNegativeNumbers(arr)).toEqual([0, 11, 34, 3, 4, 5])
         expect(filterNegativeNumbers([0])).toEqual([0])
         expect(functionalFilterNegativeNumbers([0])).toEqual([0])
     })
@@ -166,18 +57,18 @@ describe('generateRandomColor', () => {
     })
 
     it('should not modify the original array', () => {
-        const arr = [-1, 0, 1]
+        const arr = [-5, 0, 5]
         const arrCopy = JSON.parse(JSON.stringify(arr))
         filterNegativeNumbers(arr)
         expect(arr).toEqual(arrCopy)
     })
-})*/
+});
 
-/*describe('mapNumbersIntoStrings', () => {
+describe('mapNumbersIntoStrings', () => {
     it('should return a new array with strings', () => {
-        const arr = [-1, 0, 1]
-        expect(mapNumbersIntoStrings(arr)).toEqual(['-1', '0', '1'])
-        expect(functionalMapNumbersIntoStrings(arr)).toEqual(['-1', '0', '1'])
+        const arr = [-5, 0, 5]
+        expect(mapNumbersIntoStrings(arr)).toEqual(['-5', '0', '5'])
+        expect(functionalMapNumbersIntoStrings(arr)).toEqual(['-5', '0', '5'])
     })
 
     it('should return an empty array if the original array is empty', () => {
@@ -186,14 +77,14 @@ describe('generateRandomColor', () => {
     });
 
     it('should not modify the original array', () => {
-        const arr = [-1, 0, 1]
+        const arr = [-5, 0, 5]
         const arrCopy = JSON.parse(JSON.stringify(arr))
         mapNumbersIntoStrings(arr)
         expect(arr).toEqual(arrCopy)
     })
-})*/
+});
 
-/*describe('printType', () => {
+describe('printType', () => {
     beforeEach(() => {
         global.console = {
             log: jest.fn()
@@ -205,11 +96,11 @@ describe('generateRandomColor', () => {
         expect(global.console.log).toHaveBeenCalledWith('number')
     })
 
-})*/
+});
 
-/*describe('isPalindrome', () => {
+describe('isPalindrome', () => {
     it('should return true with a palindrome word', () => {
-        expect(isPalindrome('madam')).toBeTruthy()
+        expect(isPalindrome('oso')).toBeTruthy()
     })
 
     it('should return true with a palindrome phrase', () => {
@@ -219,13 +110,13 @@ describe('generateRandomColor', () => {
     it('should return true with a palindrome phrase', () => {
         expect(isPalindrome("%AniTA LAVa& LA tiNA")).toBeTruthy()
     })
-});*/
+});
 
-/*describe('Person', () => {
+describe('Person', () => {
     let personInstance
 
     beforeAll(() => {
-        personInstance = new Person('John', 100)
+        personInstance = new Person('Andres', 20)
 
         global.console = {
             log: jest.fn()
@@ -234,18 +125,18 @@ describe('generateRandomColor', () => {
 
     it('should return an instance with the correct values when calling it as a constructor', () => {
         expect(personInstance).toMatchObject({
-            name: 'John',
-            age: 100
+            name: 'Andres',
+            age: 20
         })
     })
 
     it('should print the Person instance name in the console', () => {
         personInstance.printName()
-        expect(global.console.log).toHaveBeenCalledWith('John')
+        expect(global.console.log).toHaveBeenCalledWith('Andres')
     })
-});*/
+});
 
-/*describe('printOutPersonAge', () => {
+describe('printOutPersonAge', () => {
     beforeEach(() => {
         global.console = {
             log: jest.fn()
@@ -253,7 +144,7 @@ describe('generateRandomColor', () => {
     })
 
     it('should print in console the age of the given instance', () => {
-        printOutPersonAge(new Person('John', 100))
-        expect(global.console.log).toHaveBeenCalledWith(100)
+        printOutPersonAge(new Person('Andres', 20))
+        expect(global.console.log).toHaveBeenCalledWith(20)
     })
-})*/
+});
